0502ab1bce177e51b6b5692b0c935659b6ad9b55
Vladimir <98771978+VladIn4e@users.noreply.github.com>
2022-05-07 20:25:44 +0400

    Hungru

--- main.py
+++ main.py
@@ -11,10 +11,10 @@ clock = pg.time.Clock()
 
 m = 1
 move = 1
-hunger = 0
 ress = []
 cages = []
 cages_res = []
+hungers= []
 res_pos = randrange(100, 1100), randrange(100, 700)
 gravity = 1
 
@@ -27,6 +27,7 @@ def create_cage(pos):
     cage_res = 0
     cages.append(cage)
     cages_res.append(cage_res)
+    hungers.append(0)
 
 def create_res(kol):
     for i in range(kol):
@@ -35,7 +36,7 @@ def create_res(kol):
 
 def update_res():
     for i in range(len(ress)):
-        ress[i] = pg.draw.circle(win, (0, 255, 0), (randrange(500, 700), randrange(300, 500)), 2)
+        ress[i] = pg.draw.circle(win, (0, 255, 0), (ress[i].x+2, ress[i].y+2), 2)
 
 
 def create_res_in_pos(pos):
@@ -56,117 +57,86 @@ def opt_x_and_y(x,y):
 
 def update():
     global x, y, cage_res, sun_x, sun_y, res_x, res_y, opt_x, opt_y, move, hunger, m, delete_res, delete_cage
-    print(len(cages))
     if len(cages) > 0:
+        delete_cage = []
+        delete_res = []
         for i in range(len(cages)):
-            delete_cage = []
-            for r in range(len(ress)):
-                delete_res = []
-                sun_x, sun_y, r1, r2 = create_sun((WIDTH // 2, HEIGHT // 2))
-                x, y, cage_res = cages[i].x, cages[i].y, cages_res[i]
-                res_x, res_y = ress[r].x, ress[r].y
-                opt_x, opt_y, re = opt_x_and_y(x, y)
-                if m == 1000:
-                    create_res(100)
-                    m = 0
-                if hunger < 10000:
-                    if cage_res == 1:
-                        '''
-                        if sun_x > x:
-                            x += gravity
-                        elif sun_x < x:
-                            x += -gravity
-        
-                        if sun_y > y:
-                            y += gravity
-                        elif sun_y < y:
-                            y += -gravity
-        
-                        if x < sun_x + 5 and x > sun_x - 5 and y > sun_y - 5 and y < sun_y + 5:
-                            cages_res[i] = 0
-                            print('Cage index:', i, 'put res.')
-        
-                        if x <= 0:
-                            x = 1100
-                        elif x >= 1200:
-                            x = 100
-        
-                        if y <= 0:
-                            y = 700
-                        elif y >= 800:
-                            y = 100
-                        #ress[r] = pg.draw.circle(win, (0, 255, 0), (res_x + 2, res_y + 2), 2)
-                        '''
-                        hunger = 0
-                        cages_res[i] = 0
-                        print('Cage index:', i, 'eat res in position:', x, y)
-                    else:
-                        if opt_x > x:
-                            x += gravity
-                        elif opt_x < x:
-                            x += -gravity
-
-                        if opt_y > y:
-                            y += gravity
-                        elif opt_y < y:
-                            y += -gravity
-
-                        if x < opt_x+5 and x > opt_x-5 and y > opt_y-5 and y < opt_y+5:
-                            cages_res[i] = 1
-                            if ress[re] in delete_res:
-                                pass
-                            else:
-                                delete_res.append(re)
-                            #ress[re] = pg.draw.circle(win, (0, 255, 0), (randrange(100, 1100), randrange(100, 700)), 2)
-                            print('Cage index:', i, 'take res in position:', opt_x, opt_y)
-                        #else:
-                            #ress[r] = pg.draw.circle(win, (0, 255, 0), (res_x+2, res_y+2), 2)
-
-                        if x <= 0:
-                            x = 1100
-                        elif x >= 1200:
-                            x = 100
-
-                        if y <= 0:
-                            y = 700
-                        elif y >= 800:
-                            y = 100
-                        #hunger += 1
-                    ress[r] = pg.draw.circle(win, (0, 255, 0), (res_x+2, res_y+2), 2)
+            x, y, cage_res, hunger = cages[i].x, cages[i].y, cages_res[i], hungers[i]
+            opt_x, opt_y, re = opt_x_and_y(x, y)
+            if m == 100:
+                create_res(300)
+                m = 0
+            if hunger < 100:
+                if cage_res == 1:
+                    hungers[i] = 0
+                    cages_res[i] = 0
+                    print('Cage index:', i, 'eat res in position:', x, y)
                 else:
-                    if cages[i] in delete_cage:
-                        pass
-                    else:
-                        delete_cage.append(i)
-                #print(ress)
-            #print(i)
-                #print('Res position:', res_x, res_y, 'P:', x, y, 'And opt:', opt_x, opt_y)
-            #print('Planet_index:', planet_index)
+                    if opt_x > x:
+                        x += gravity
+                    elif opt_x < x:
+                        x += -gravity
+
+                    if opt_y > y:
+                        y += gravity
+                    elif opt_y < y:
+                        y += -gravity
+
+                    if x < opt_x+5 and x > opt_x-5 and y > opt_y-5 and y < opt_y+5:
+                        cages_res[i] = 1
+                        print('Cage index:', i, 'take res in position:', opt_x, opt_y)
+                        if ress[re] in delete_res:
+                            pass
+                        else:
+                            print('DelR', re)
+                            delete_res.append(re)
+
+                    if x <= 0:
+                        x = 1100
+                    elif x >= 1200:
+                        x = 100
+
+                    if y <= 0:
+                        y = 700
+                    elif y >= 800:
+                        y = 100
+                hungers[i] += 1
+                update_res()
+            else:
+                if cages[i] in delete_cage:
+                    pass
+                else:
+                    print('DelC:', i)
+                    delete_cage.append(i)
             print('Move:', move, 'Planet position:', i, x, y, cages_res[i], opt_x, opt_y)
-            #print('Sun position:', sun_x, str(sun_y) + ', Res position:', res_x, res_y, 'And planet position:', x, y, cages_res[i])
-            #print('Gravity:', gravity)
-            #print('Planets:', planets[i])
             cages[i] = pg.draw.circle(win, (255, 0, 0), (x + 4, y + 4), 4)
-            move += 1
-            m += 1
-            #print('Planets:', planets[i])
+        move += 1
+        m += 1
 
         for i in range(len(delete_res)):
             print('D_r:', i)
-            ress.pop(delete_res[i])
+            print(delete_res[i])
+            try:
+                ress.pop(delete_res[i])
+            except IndexError:
+                ress.pop()
 
         for i in range(len(delete_cage)):
             print('D_c:', i)
-            cages.pop(delete_cage[i])
+            print(delete_cage[i])
+            try:
+                cages.pop(delete_cage[i])
+            except IndexError:
+                cages.pop()
 
     else:
         print('Dead!')
         exit()
 
-for i in range(10):
-    create_cage((randrange(100, 1100), randrange(100, 700)))
+for i in range(20):
+    create_cage((randrange(400, 800), randrange(200, 600)))
 
-create_res(100)
+create_res(300)
 
 while True:
     for i in pg.event.get():
